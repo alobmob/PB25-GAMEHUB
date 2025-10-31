aafa234d410a87948896bb387636226f07ae5321
alobmob <welds-natural1n@icloud.com>
2025-10-31 21:08:28 +0100

    fix(client): usunieto import home.css z main

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,6 @@ import router from './router';
 import pinia from './stores';
 import './assets/main.css';
 import './assets/components.css';
-import './assets/home.css';
+// import './assets/home.css';
 
 createApp(App).use(pinia).use(router).mount('#app');
